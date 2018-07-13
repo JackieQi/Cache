eb24eedcb55df4d93a600787df596c8e74bbe51d
JackieQi <superqichi@gmail.com>
2018-07-12 21:56:35 -0700

    replace nscache with dic

--- Source/Shared/Storage/MemoryStorage.swift
+++ Source/Shared/Storage/MemoryStorage.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public class MemoryStorage<T>: StorageAware {
-  fileprivate let cache = NSCache<NSString, MemoryCapsule>()
+  fileprivate var cache: [String: MemoryCapsule] =  [:]
   // Memory cache keys
   fileprivate var keys = Set<String>()
   /// Configuration
@@ -9,20 +9,22 @@ public class MemoryStorage<T>: StorageAware {
 
   public init(config: MemoryConfig) {
     self.config = config
-    self.cache.countLimit = Int(config.countLimit)
-    self.cache.totalCostLimit = Int(config.totalCostLimit)
+    //self.cache.countLimit = Int(config.countLimit)
+    //self.cache.totalCostLimit = Int(config.totalCostLimit)
   }
 }
 
 extension MemoryStorage {
   public func setObject(_ object: T, forKey key: String, expiry: Expiry? = nil) {
     let capsule = MemoryCapsule(value: object, expiry: .date(expiry?.date ?? config.expiry.date))
-    cache.setObject(capsule, forKey: NSString(string: key))
+    //cache.setObject(capsule, forKey: NSString(string: key))
+    cache[key] = capsule
     keys.insert(key)
   }
 
   public func removeAll() {
-    cache.removeAllObjects()
+    cache.removeAll()
+    //cache.removeAllObjects()
     keys.removeAll()
   }
 
@@ -34,18 +36,20 @@ extension MemoryStorage {
   }
 
   public func removeObjectIfExpired(forKey key: String) {
-    if let capsule = cache.object(forKey: NSString(string: key)), capsule.expiry.isExpired {
+    if let capsule = cache[key], capsule.expiry.isExpired {
       removeObject(forKey: key)
     }
   }
 
   public func removeObject(forKey key: String) {
-    cache.removeObject(forKey: NSString(string: key))
+    cache.removeValue(forKey: key)
+    //cache.removeObject(forKey: NSString(string: key))
     keys.remove(key)
   }
 
   public func entry(forKey key: String) throws -> Entry<T> {
-    guard let capsule = cache.object(forKey: NSString(string: key)) else {
+    guard let capsule = cache[key] else {
+      //guard let capsule = cache.object(forKey: NSString(string: key)) else {
       throw StorageError.notFound
     }
 
